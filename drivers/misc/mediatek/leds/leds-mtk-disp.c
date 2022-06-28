4f476aa79bd1d7b41c5c1999cc000e2d2e0c786b
LinkBoi00 <linkdevel@protonmail.com>
2022-06-29 01:36:14 +0300

    drivers: misc/mtk: leds: Import Xiaomi changes
    Taken from cannon-r-oss
    
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- drivers/misc/mediatek/leds/leds-mtk-disp.c
+++ drivers/misc/mediatek/leds/leds-mtk-disp.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2018 MediaTek Inc.
+ * Copyright (C) 2021 XiaoMi, Inc.
  *
  */
 
@@ -18,6 +19,9 @@
 
 #include "leds-mtk-disp.h"
 
+#ifdef CONFIG_BACKLIGHT_SUPPORT_LM36273
+extern int lm36273_brightness_set(int level);
+#endif
 
 #ifdef CONFIG_DRM_MEDIATEK
 extern int mtkfb_set_backlight_level(unsigned int level);
@@ -174,8 +178,15 @@ static int led_level_disp_set(struct mtk_led_data *s_led,
 	if (enable_met_backlight_tag())
 		output_met_backlight_tag(brightness);
 #endif
-#ifdef CONFIG_DRM_MEDIATEK
+// lm36273 set backlight
+#ifdef CONFIG_BACKLIGHT_SUPPORT_LM36273
+	pr_debug("%s lm36273 set brightness: %d\n", __func__, brightness);
+	lm36273_brightness_set(brightness);
+#else
 	mtkfb_set_backlight_level(brightness);
+#endif
+
+#ifdef CONFIG_DRM_MEDIATEK
 	s_led->conf.level = brightness;
 #endif
 	return 0;
@@ -248,6 +259,7 @@ int mt_leds_brightness_set(char *name, int level)
 }
 EXPORT_SYMBOL(mt_leds_brightness_set);
 
+extern void mtk_notify_backlight_node(unsigned int backlight);
 static int led_level_set(struct led_classdev *led_cdev,
 					  enum led_brightness brightness)
 {
@@ -258,14 +270,18 @@ static int led_level_set(struct led_classdev *led_cdev,
 	struct mtk_led_data *led_dat =
 		container_of(led_conf, struct mtk_led_data, conf);
 
+	pr_debug("backlight = %d, last backlight = %d", brightness, led_dat->brightness);
 	if (led_dat->brightness == brightness)
 		return 0;
+
+	sysfs_notify(&led_cdev->dev->kobj, NULL, "brightness");
 
 	trans_level = (
 		(((1 << led_dat->conf.trans_bits) - 1) * brightness
 		+ (((1 << led_dat->conf.led_bits) - 1) / 2))
 		/ ((1 << led_dat->conf.led_bits) - 1));
 
+	pr_debug("backlight = %d, trans_level backlight = %d", brightness, trans_level);
 	led_debug_log(led_dat, brightness, trans_level);
 
 #ifdef MET_USER_EVENT_SUPPORT
@@ -283,8 +299,9 @@ led_dat->brightness = brightness;
 	led_level_disp_set(led_dat, brightness);
 	led_dat->last_level = brightness;
 #endif
-	return 0;
 
+	mtk_notify_backlight_node(brightness);
+	return 0;
 }
 
 static int led_data_init(struct device *dev, struct mtk_led_data *s_led)
