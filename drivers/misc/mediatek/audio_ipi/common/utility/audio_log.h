4c4ad6d915bf9367dc2703cb403225bb6b4d7815
LinkBoi00 <linkdevel@protonmail.com>
2023-01-01 22:13:57 +0200

    drivers: misc/mtk: audio_ipi/audio_scp: Tone down debug and verbose logging
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- drivers/misc/mediatek/audio_ipi/common/utility/audio_log.h
+++ drivers/misc/mediatek/audio_ipi/common/utility/audio_log.h
@@ -30,11 +30,11 @@
 #endif
 
 #ifndef AUD_LOG_W
-#define AUD_LOG_W pr_info
+#define AUD_LOG_W pr_debug
 #endif
 
 #ifndef AUD_LOG_E
-#define AUD_LOG_E pr_notice
+#define AUD_LOG_E pr_err
 #endif
 
 #endif /* end of AUDIO_LOG_H */
